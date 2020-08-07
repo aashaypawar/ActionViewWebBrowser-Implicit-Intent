efc0622113b2b497a00553107c14701811878910
Aashay <Aashay@Aashays-MacBook-Air.local>
2020-08-08 00:15:29 +0530

    Initial commit

---
+++ app/src/main/java/com/example/actionviewwebbrowser_implicit_intent/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.actionviewwebbrowser_implicit_intent
+
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btn = findViewById<Button>(R.id.btn)
+
+        btn.setOnClickListener {
+            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("https://www.nasa.gov")))
+        }
+    }
+}
